7001eec60d9bba00ca93a99b5fd756ec2f764dd7
cmkm3010 <cmkm8384@gmail.com>
2023-12-20 10:38:27 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>📗노트</h1>
+        <button>쓰기</button>
+      </div>
+      <ul className="list">
+        <li>
+          <h2>노트 제목...</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus, error sequi earum natus eos reiciendis voluptatum
+            doloremque explicabo sed, iusto neque esse dicta. Excepturi,
+            molestias doloremque. Nam, incidunt commodi. Ab!
+          </p>
+          <div className="note_footer">
+            <div className="date">2023.12.20 10:35:40</div>
+            <div className="btns">
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목...</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus, error sequi earum natus eos reiciendis voluptatum
+            doloremque explicabo sed, iusto neque esse dicta. Excepturi,
+            molestias doloremque. Nam, incidunt commodi. Ab!
+          </p>
+          <div className="note_footer">
+            <div className="date">2023.12.20 10:35:40</div>
+            <div className="btns">
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목...</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus, error sequi earum natus eos reiciendis voluptatum
+            doloremque explicabo sed, iusto neque esse dicta. Excepturi,
+            molestias doloremque. Nam, incidunt commodi. Ab!
+          </p>
+          <div className="note_footer">
+            <div className="date">2023.12.20 10:35:40</div>
+            <div className="btns">
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목...</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus, error sequi earum natus eos reiciendis voluptatum
+            doloremque explicabo sed, iusto neque esse dicta. Excepturi,
+            molestias doloremque. Nam, incidunt commodi. Ab!
+          </p>
+          <div className="note_footer">
+            <div className="date">2023.12.20 10:35:40</div>
+            <div className="btns">
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목...</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus, error sequi earum natus eos reiciendis voluptatum
+            doloremque explicabo sed, iusto neque esse dicta. Excepturi,
+            molestias doloremque. Nam, incidunt commodi. Ab!
+          </p>
+          <div className="note_footer">
+            <div className="date">2023.12.20 10:35:40</div>
+            <div className="btns">
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목...</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus, error sequi earum natus eos reiciendis voluptatum
+            doloremque explicabo sed, iusto neque esse dicta. Excepturi,
+            molestias doloremque. Nam, incidunt commodi. Ab!
+          </p>
+          <div className="note_footer">
+            <div className="date">2023.12.20 10:35:40</div>
+            <div className="btns">
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
