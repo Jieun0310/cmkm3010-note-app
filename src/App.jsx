705300a16c3e12a4dbc0dd3858c0a4a36b2d1e75
cmkm3010 <cmkm8384@gmail.com>
2023-12-21 11:04:01 +0900

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import header from './components/header';
-import { db } from './firebase';
+
 import List from './page/List';
 import Header from './components/header';
 
 export default function App() {
-  console.log(db);
-
   return (
     <div className="App">
       <Header />
